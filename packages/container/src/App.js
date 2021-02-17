1bb7c18134343e8276320b261c592a741cd20c0a
Amarnath <sharewithamar@gmail.com>
2021-02-17 19:01:17 +0530

    Navigation logic, public path, lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header';
+import Progress from './components/progress';
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
+//import MarketingApp from './components/MarketingApp';
+//import AuthApp from './components/AuthApp';
+
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
@@ -16,7 +24,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
